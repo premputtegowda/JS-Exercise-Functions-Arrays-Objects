dbfebe1c08f2d6bd3be852355557dcad417a400d
Prem Puttegowda <puttegowdaprem@gmail.com>
2019-11-11 14:40:24 -0800

    Completed the stretch goal

--- index.js
+++ index.js
@@ -336,8 +336,15 @@ const argTimesTwo = () => num*2; // code here!
  *         (1) causes the odometer in the object to be increased by the distance,
  *         (2) returns the updated value of the `odometer`.
 */
-function carMaker(/* code here */) {
+function carMaker(odometer) {
   /* code here */
+  return {
+    odometer: odometer,
+    drive: function (distance) {
+        this.odometer += distance;
+        return this.odometer;
+    }
+  }
 }
 
 /// ////// END OF CHALLENGE /////////
